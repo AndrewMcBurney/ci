d614252c67baefd69856b2d98f86403df42cd041
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-03-05 22:33:42 -0500

    WIP

--- features/credentials/provider_credentials_controller.rb
+++ features/credentials/provider_credentials_controller.rb
@@ -8,6 +8,11 @@ module FastlaneCI
   class ProviderCredentialsController < AuthenticatedControllerBase
     HOME = "/provider_credentials"
 
+    get HOME do
+      locals = { title: "Provider Credentials" }
+      erb(:provider_credentials, locals: locals, layout: FastlaneCI.default_layout)
+    end
+
     post "#{HOME}/create" do
       if valid_params?(params, provider_credential_params)
         Services.provider_credential_service.create_provider_credential!(
@@ -15,7 +20,7 @@ module FastlaneCI
         )
       end
 
-      redirect("/users")
+      redirect(HOME)
     end
 
     post "#{HOME}/update" do
@@ -25,10 +30,31 @@ module FastlaneCI
         )
       end
 
-      redirect("/users")
+      redirect(HOME)
     end
 
     private
+
+    #####################################################
+    # @!group Data: View-specific data
+    #####################################################
+
+    # @return [Hash]
+    def user_credentials
+      Services
+        .user_service
+        .user_data_source
+        .users
+        .map { |user| [user.id, user.provider_credentials] }
+        .to_h
+    end
+
+    # Empty provider credential
+    #
+    # @return [GitHubProviderCredential]
+    def new_credential
+      @new_credential ||= GitHubProviderCredential.new
+    end
 
     #####################################################
     # @!group Locals: View-specific locals
@@ -38,5 +64,7 @@ module FastlaneCI
     def provider_credential_params
       Set.new(%w(user_id id email api_token provider_name type full_name))
     end
+
+    # TODO: validate that this works lol
   end
 end
